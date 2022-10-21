17debcefb496ffe393d9046bd248430287a8e35a
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-10-21 15:18:28 -0300

    chore: add rust to ci

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DataParser.MixProject do
   def project do
     [
       app: :netcdf,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
